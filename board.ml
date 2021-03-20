6c4fe013ec20fe49897a2344e7e5d377c97204b0
Sunwook Kim <sk2676@cornell.edu>
2021-03-20 16:07:29 -0400

    Alot of new drafts

--- board.ml
+++ board.ml
@@ -1,15 +1,39 @@
-(*TODO: Implement something*)
-type t 
+type tile ={
+  letter : char; (* If no letter tile is placed, char is '.'*)
+  coor : int * int;
+  adjacent_tiles : tile list;
+}
+(*-1 -1 -1 -1 -1 
+  -1 -1 L -1  -1
+  -1 -1 E -1  -1
+  -1 -1 A -1  -1
+  -1 -1 F -1  -1*)
+ (* The grids implement by 2 dimensional array*)
+type t = {
+  n:int (* Dimension of the board*);
+  (* n x n array of tile*)
+  tile_board = (* Take cares of tile placement on board n x n*);
+  info_board = (*Take cares of board info n x n (double score) TODO: someday*);
+}
 
+(*
+1) implementation of type t 1.5) to_string
+2) test 
+*)
 let place_tiles = failwith "unimplemented"
 
+(* Show the current string in pretty print*)
 let to_string = failwith "unimplemented"
 
 (** Helper function to check if word is in dictionary*)
 let word_in_dict word dict = failwith "unimplemnted"
 
 (** Helper function to check if the tile placement is near a current tile*)
-let tiles_near_current_tiles =failwith "unimplemnted" 
+let tiles_near_current_tiles = failwith "unimplemnted" 
 
 (** Use the two helper functions above to check if a placement is legal*)
 let placement_is_legal t placement =failwith "unimplemnted" 
+
+(*Return a new board from json*)
+let from_json json = failwith "unimplemented"
+(* Score stuff *)
--- game.ml
+++ game.ml
@@ -1,9 +1,29 @@
 (** TODO: This is an incomplete implementation *)
 type dictionary = string list
 (** TODO: This is an incomplete implementation *)
-type t = { board: Board.t; dict: dictionary}
+type t = { 
+  board: Board.t; 
+  dict: dictionary;
+  (*
+  hand: ;
+  scores:[0,1,2,]
+  tilepool:
+    shuffle (private), draw x tiles (public), is_emtpy, initialization
+  *)
+}
 
 (** TODO: Change according to input of Board.place_tiles *)
 let place_tiles = Board.place_tiles t.board ()
 
+(** GIBBERISH: erase when want to
+let play_turn 
+  prompt
+    player_one_plays (something on somethingwhere)
 
+
+player_one_plays 
+  board.put something 
+  |> change_score 
+  |> 
+
+*)
