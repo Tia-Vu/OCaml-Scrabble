0ca3e370c695d470193a50c28a90aae50d18d1d7
Sunwook Kim <sk2676@cornell.edu>
2021-03-21 21:33:30 -0400

    Implement board type and its initialization

--- board.ml
+++ board.ml
@@ -1,25 +1,42 @@
-type tile ={
-  letter : char; (* If no letter tile is placed, char is '.'*)
+type tile = {
+  letter : char;
+  (* If no letter tile is placed, char is '.'*)
+  (* -1, -1 if empty tile. Else, the coordinate of the letter tile*)
   coor : int * int;
-  adjacent_tiles : tile list;
-}
-(*-1 -1 -1 -1 -1 
-  -1 -1 L -1  -1
-  -1 -1 E -1  -1
-  -1 -1 A -1  -1
-  -1 -1 F -1  -1*)
- (* The grids implement by 2 dimensional array*)
-type t = {
-  n:int (* Dimension of the board*);
-  (* n x n array of tile*)
-  tile_board = (* Take cares of tile placement on board n x n*);
-  info_board = (*Take cares of board info n x n (double score) TODO: someday*);
+  adjacent_tiles : tile list; (* Adjacent letter tiles - not empty ones*)
 }
 
-(*
-1) implementation of type t 1.5) to_string
-2) test 
-*)
+(** [itile] is an information tile for the info_board TODO: Bonus should
+    be more sophisticated *)
+type itile = { bonus : int }
+
+type t = {
+  n : int;
+  (* Dimension of the board*)
+  (* Take cares of tile placement on board n x n. tile_board[row][col]*)
+  tile_board : tile array array;
+  (*Take cares of board info n x n (double score) TODO: someday*)
+  info_board : itile array array;
+}
+
+let create_tile l x y =
+  { letter = l; coor = (x, y); adjacent_tiles = [] }
+
+let init_tile () = create_tile '.' (-1) (-1)
+
+let init_itile b = { bonus = b }
+
+let init_tile_board n =
+  let init_row n = Array.make n (init_tile ()) in
+  Array.make n (init_row n)
+
+let init_info_board n =
+  let init_row n = Array.make n (init_itile 0) in
+  Array.make n (init_row n)
+
+let init_board n =
+  { n; tile_board = init_tile_board n; info_board = init_info_board n }
+
 let place_tiles = failwith "unimplemented"
 
 (* Show the current string in pretty print*)
@@ -28,12 +45,14 @@ let to_string = failwith "unimplemented"
 (** Helper function to check if word is in dictionary*)
 let word_in_dict word dict = failwith "unimplemnted"
 
-(** Helper function to check if the tile placement is near a current tile*)
-let tiles_near_current_tiles = failwith "unimplemnted" 
+(** Helper function to check if the tile placement is near a current
+    tile*)
+let tiles_near_current_tiles = failwith "unimplemnted"
 
 (** Use the two helper functions above to check if a placement is legal*)
-let placement_is_legal t placement =failwith "unimplemnted" 
+let placement_is_legal t placement = failwith "unimplemnted"
 
 (*Return a new board from json*)
 let from_json json = failwith "unimplemented"
+
 (* Score stuff *)
