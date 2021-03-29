07b125dd355d788878c2343ef1279bbd5f355db1
Sunwook Kim <sk2676@cornell.edu>
2021-03-28 22:50:52 -0400

    Make board to_string and remove _build files

--- _build/score.ml
+++
@@ -1,5 +0,0 @@
-type t = int list list
-
-let create () = failwith "unimplemented"
-
-let update_score scores score_to_add = failwith "unimplemented"
--- board.ml
+++ board.ml
@@ -38,24 +38,41 @@ let init_info_board n =
 let init_board n =
   { n; tile_board = init_tile_board n; info_board = init_info_board n }
 
-let empty_board j = failwith "unimplemented"
+let empty_board j = init_board 1 (*TODO: Placeholder*)
 
-let place_tiles = failwith "unimplemented"
+let place_tiles x = failwith "unimplemented"
 
-(* Show the current string in pretty print*)
-let to_string = failwith "unimplemented"
+let row_to_string row =
+  let add_letter str t = str ^ " " ^ Char.escaped t.letter in
+  let spaced_str = Array.fold_left add_letter "" row in
+  String.sub spaced_str 1 (String.length spaced_str - 1)
+
+(*Array.fold_left add_letter "" row*)
+
+let to_string b =
+  let rows = Array.map row_to_string b.tile_board in
+  (*String.sub (Array.fold_left ( ^ ) "" rows) 1 (b.n - 1)*)
+  let add_row str row = str ^ "\n" ^ row in
+  let entered_str = Array.fold_left add_row "" rows in
+  String.sub entered_str 1 (String.length entered_str - 1)
 
 (** Helper function to check if word is in dictionary*)
-let word_in_dict word dict = failwith "unimplemnted"
+let word_in_dict word dict = ()
+
+(*failwith "unimplemnted"*)
 
 (** Helper function to check if the tile placement is near a current
     tile*)
-let tiles_near_current_tiles = failwith "unimplemnted"
+let tiles_near_current_tiles = ()
+
+(*failwith "unimplemnted"*)
 
 (** Use the two helper functions above to check if a placement is legal*)
-let placement_is_legal t placement = failwith "unimplemnted"
+let placement_is_legal t placement = ()
+
+(*failwith "unimplemnted"*)
 
 (*Return a new board from json*)
-let from_json json = failwith "unimplemented"
+let from_json json = () (*failwith "unimplemented"*)
 
-(* Score stuff *)
+                        (* Score stuff *)
