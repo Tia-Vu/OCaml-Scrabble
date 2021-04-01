54dfa65d152a678fe4e413039698c8fb947b1a57
Sunwook Kim <sk2676@cornell.edu>
2021-03-31 21:29:27 -0400

    Add authors

---
+++ authors.mli
@@ -0,0 +1,7 @@
+(** CS 3110 Spring 2021 Project
+
+    @authors Tia Vu (tnv5), Sophia Chen (sc2329), Sunwook Kim (sk2676) *)
+
+(** [hours_worked] is the sum of hours we each worked on this
+    assignment. *)
+val hours_worked : int
