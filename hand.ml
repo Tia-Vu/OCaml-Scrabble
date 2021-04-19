49a82f936f0048a92fef4adb0d9ca45e8f2068ea
Sophia Chen <sc2329@cornell.edu>
2021-04-19 17:59:11 -0400

    Deleted unnecessary comments

--- hand.ml
+++ hand.ml
@@ -1,14 +1,3 @@
-(* type t = unit
-
-   type move_possibility = | Possible of t | Impossible
-
-   let make_move hand move = failwith "Unimplemented"
-
-   let new_hand pool = failwith "Unimplemented"
-
-   let to_list hand = failwith "Unimplemented" *)
-(*Above is mine, below is sunwook's*)
-
 type t = char list
 
 let empty_hand () = []
--- hand.mli
+++ hand.mli
@@ -1,24 +1,3 @@
-(** This module represents the current hand of tiles of a player in a
-    scrabble game. *)
-
-(* (** The abstract type of values representing a hand. *) type t
-
-   (**A certain move can either be [Possible] or [Impossible] based on a
-   certain hand.*) type move_possibility = | Possible of t | Impossible
-
-   (**[make_move] takes in the original hand and the word to be taken
-   from the hand, and is [Possible] with the new hand if the move is
-   legal (the word can be made with tiles in the hand) and [Impossible]
-   if the move is not legal (the word cannot be made with the tiles in
-   the hand).*) val make_move : t -> string -> move_possibility
-
-   (**new_hand is a new random hand taken from the tiles of a passed in
-   tile pool.*) val new_hand : Tilepool.t -> t
-
-   (**[to_list] is the hand but as a string list of each tile in the
-   hand.*) val to_list : t -> string list *)
-(*Compare both, above is mine*)
-
 (** Representation of a player's hand in the game*)
 open Pool
 
