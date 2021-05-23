9c9a07afb7ad6a5d1743ce081778550ef3d93b68
Sunwook Kim <sk2676@cornell.edu>
2021-05-23 13:43:07 -0400

    Revised hand.mli

--- hand.mli
+++ hand.mli
@@ -1,28 +1,24 @@
-(** Representation of a player's hand in the game*)
+(** Representation of a player's hand in the game.*)
 open Pool
 
-(** The abstract type of values represnting hand*)
+(** The abstract type of values represnting a plyaer's hand.*)
 type t = char list
 
-(** [empty_hand ()] is an empty hand.*)
-
+(** [empty_hand ()] gives an empty hand.*)
 val empty_hand : unit -> t
 
-(** [size hand] is the size of the hand.*)
-
+(** [size hand] gives the size of [hand].*)
 val size : t -> int
 
-(** [draw_nletters pool n hand] is a hand created from drawing [n]
-    letters from [pool] to hand. If there are less than [n] letters in
+(** [draw_nletters pool n hand] gives a hand created after drawing [n]
+    letters from [pool] to [hand]. If there are less than [n] letters in
     [pool], draw as much as possible.
 
-    Requires: n >= 0 *)
-
+    Requires: [n >= 0] *)
 val draw_nletters : Pool.t -> int -> t -> t
 
 (** [has_word word hand] is [true] if and only if [hand] has the letters
     to make [word].*)
-
 val has_word : char list -> t -> bool
 
 (** The exception [InsufficientTiles] is raised when there are not
@@ -32,15 +28,13 @@ exception InsufficentTiles
 (** [spend_word word hand] is the new hand after spending the
     appropriate letter tiles from [hand] to create [word].
 
-    Raises: InsufficientTiles *)
-
+    Raises: InsufficientTiles if [hand] does not have enough letter
+    tiles to create [word]. *)
 val spend_word : char list -> t -> t
 
-(**[fill_hand pool max hand] is the hand created from drawing letters
-   from [pool] and adds to the old [hand] until the [max] number of
-   tiles in the hand is reached.*)
+(**[fill_hand pool max hand] is the hand created after drawing letters
+   from [pool] to [hand] until it has [max] number of tiles.*)
 val fill_hand : Pool.t -> int -> t -> t
 
-(** [to_string hand] is a string representation of a hand.*)
-
+(** [to_string hand] is the string representation of [hand].*)
 val to_string : t -> string
