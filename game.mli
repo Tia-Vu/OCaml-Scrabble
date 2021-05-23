8ab5b32ec5f19e24f01cd23205b6d1c857078fe9
Sunwook Kim <sk2676@cornell.edu>
2021-05-23 13:42:59 -0400

    Revised game.mli

--- game.mli
+++ game.mli
@@ -1,6 +1,4 @@
-(** Representation of a scrabble game*)
+(** Representation of a scrabble game.*)
 
-(** The abstract type of values representing a scrabble game. *)
 (** [run ()] runs the scrabble game. *)
-
 val run : unit -> 'a
