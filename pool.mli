e7b62d3f7a166dfae6110cd18c57435a6b786d24
Sunwook Kim <sk2676@cornell.edu>
2021-05-23 13:43:19 -0400

    revised pool.mli

--- pool.mli
+++ pool.mli
@@ -1,28 +1,26 @@
-(** Representation of tile pool of the current game.*)
+(** Representation of the tile pool of the current game.*)
 
 (** The abstract type of values represnting tile pools.*)
 type t
 
-(** The exception [DrawFromEmpty] is raised when a letter is attempted
-    to be drawn from an empty pool. *)
+(** Exception [DrawFromEmpty] is raised when a letter is attempted to be
+    drawn from an empty pool. *)
 exception DrawFromEmpty
 
-(** [init_pool ()] is a new tile pool.*)
+(** [init_pool ()] gives a new tile pool.*)
 val init_pool : unit -> t
 
-(**[size p] is the size of the current tile_pool*)
+(**[size pool] gives the size of [pool].*)
 val size : t -> int
 
-(**[is_empty p] is true if and only if [p] is empty.*)
+(**[is_empty pool] is true if and only if [pool] is empty.*)
 val is_empty : t -> bool
 
-(* TODO: (to think about) Should we draw tiles instead of letters? *)
+(**[draw_letter pool] draws a random letter from [pool].
 
-(**[draw_letter p] is a random letter from pool [p]
-
-   Requires: [is_empty p] = false*)
+   Requires: [is_empty pool = false].*)
 val draw_letter : t -> char
 
-(** [to_string p] converts pool [p] to a string, which shows the number
-    of tiles in the pool.*)
+(** [to_string pool] converts [pool] to a string, which shows the number
+    of tiles in [pool].*)
 val to_string : t -> string
